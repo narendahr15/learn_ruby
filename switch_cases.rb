4b5d803c4ba73ccf69b1cd004cb7b436fff69443
narendahr15 <narendhar15@gmail.com>
2017-04-10 11:55:08 +0530

    Switch case usage

---
+++ switch_cases.rb
@@ -0,0 +1,26 @@
+def grade_marks(a)
+    case a
+    when 0..39
+        puts "F"
+    when 40..50
+        puts "E"
+    when 50..60
+        puts "D"
+    when 60..70
+        puts "C"
+    when 70..80
+        puts "B"
+    when 80..90
+        puts "A"
+    when 90..100
+        puts "A+"
+    else
+        puts "Not Valid"
+    end
+end
+
+grade_marks(23)
+grade_marks(101)
+grade_marks("sdf")
+        
+            
